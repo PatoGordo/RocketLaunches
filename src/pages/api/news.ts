0688f39f93990093d0eb4ccbc07854c7d6d1525c
PatoGordo <59514690+PatoGordo@users.noreply.github.com>
2021-12-04 09:58:19 -0300

    Update news.ts

--- src/pages/api/news.ts
+++ src/pages/api/news.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if(req.method === 'GET') {
 
-    const myRequest = await axios.get('https://www.spaceflightnewsapi.net/api/v2/articles?_limit=4')
+    const myRequest = await axios.get('https://api.spaceflightnewsapi.net/v3/articles?_limit=4')
     res.setHeader('Cache-Control', 's-maxage=35, stale-while-revalidate')
     
     return res.json({
